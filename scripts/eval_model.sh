875be9c052a40da0866e20d6972ba71c20f90364
Cenk Bircanoglu <cenk.bircanoglu@gmail.com>
2021-11-06 16:24:29 +0100

    add experiments

--- scripts/eval_model.sh
+++ scripts/eval_model.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-#
-# Brief description of your script
-# Copyright 2021 33755
-backbone=resnet50v1
-crop=256
 
-for model_name in seg-model-4 seg-model-49 seg-model-99 seg-model-149 seg-model-199 final-model; do
+backbone=resnet50v2
+crop=320
+
+for model_name in seg-model-4 seg-model-19 seg-model-39 final-model; do
     python -m make_cam \
-        backbone=resnet50v2 \
+        backbone=${backbone} \
         weights=$(pwd)/results/pipeline/${backbone}/crop${crop}/train/weights/${model_name}.pt \
-        hydra.run.dir=$(pwd)/results/pipeline/${backbone}/crop${crop}/train/make_cam \
+        hydra.run.dir=$(pwd)/results/pipeline/${backbone}/crop${crop}/eval/make_cam/${model_name} \
         infer_list=$(pwd)/voc12/train.txt
 
     python -m eval_cam \
-        cam_out_dir=$(pwd)/results/pipeline/${backbone}/crop${crop}/train/make_cam/cam_outputs \
-        hydra.run.dir=$(pwd)/results/pipeline/${backbone}/crop${crop}/train/eval_cam \
-        infer_set=train
+        cam_out_dir=$(pwd)/results/pipeline/${backbone}/crop${crop}/eval/make_cam/${model_name}/cam_outputs \
+        hydra.run.dir=$(pwd)/results/pipeline/${backbone}/crop${crop}/eval/eval_cam/${model_name} \
+        infer_set=train \
+        logs=$(pwd)/results/pipeline/${backbone}/crop${crop}/eval/logs \
+        last_epoch=50
 done
