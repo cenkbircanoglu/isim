a295769dfa04271fa7ed0c056a237c66dcd6444a
Cenk Bircanoglu <cenk.bircanoglu@gmail.com>
2021-11-09 08:45:42 +0100

    update the experiments

--- scripts/eval_model.sh
+++ scripts/eval_model.sh
@@ -1,19 +1,28 @@
 #!/bin/bash
 
-backbone=resnet50v2
-crop=320
+CROP_SIZE=256
 
-for model_name in seg-model-4 seg-model-19 seg-model-39 final-model; do
-    python -m make_cam \
-        backbone=${backbone} \
-        weights=$(pwd)/results/pipeline/${backbone}/crop${crop}/train/weights/${model_name}.pt \
-        hydra.run.dir=$(pwd)/results/pipeline/${backbone}/crop${crop}/eval/make_cam/${model_name} \
-        infer_list=$(pwd)/voc12/train.txt
+for BACKBONE in resnet50v1 resnet50v2 resnet50v3 resnet50v4; do
+    for model_name in seg-model-4 seg-model-24 final-model; do
+        python -m make_cam \
+            backbone=${BACKBONE} \
+            weights=$(pwd)/results/pipeline/${BACKBONE}/crop${CROP_SIZE}/train/weights/${model_name}.pt \
+            hydra.run.dir=$(pwd)/results/pipeline/${BACKBONE}/crop${CROP_SIZE}/eval/make_cam/${model_name} \
+            infer_list=$(pwd)/voc12/train.txt \
+            crop_size=${CROP_SIZE}
+    done
+done
 
-    python -m eval_cam \
-        cam_out_dir=$(pwd)/results/pipeline/${backbone}/crop${crop}/eval/make_cam/${model_name}/cam_outputs \
-        hydra.run.dir=$(pwd)/results/pipeline/${backbone}/crop${crop}/eval/eval_cam/${model_name} \
-        infer_set=train \
-        logs=$(pwd)/results/pipeline/${backbone}/crop${crop}/eval/logs \
-        last_epoch=50
+for CAM_EVAL_THRESHOLD in 0.1 0.15 0.2 0.25 0.3; do
+    for BACKBONE in resnet50v1 resnet50v2 resnet50v3 resnet50v4; do
+        for model_name in seg-model-4 seg-model-24 final-model; do
+            python -m eval_cam \
+                cam_out_dir=$(pwd)/results/pipeline/${BACKBONE}/crop${CROP_SIZE}/eval/make_cam/${model_name}/cam_outputs \
+                hydra.run.dir=$(pwd)/results/pipeline/${BACKBONE}/crop${CROP_SIZE}/eval/eval_cam/${model_name}_${CAM_EVAL_THRESHOLD} \
+                infer_set=train \
+                logs=$(pwd)/results/pipeline/${BACKBONE}/crop${CROP_SIZE}/eval/logs_${CAM_EVAL_THRESHOLD} \
+                last_epoch=50 \
+                cam_eval_thres=${CAM_EVAL_THRESHOLD}
+        done
+    done
 done
