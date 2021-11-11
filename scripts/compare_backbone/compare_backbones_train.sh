41144012689194620fc65433d066a2a34f643e28
Cenk Bircanoğlu <cenk.bircanoglu@gmail.com>
2021-11-11 09:04:48 +0100

    Train longer epochs (#9)
    * initial commit
    
    * working example
    
    * add new models
    
    * update rescaling
    
    * add eval make cam
    
    * minor updates
    
    * add experiments
    
    * update the experiments
    
    * update tests
    
    * update scripts
    
    * update scripts and scales
    
    * introduce bash script
    
    * create a bash function
    
    * initial commit
    
    * working example
    
    * add new models
    
    * add eval make cam
    
    * minor updates
    
    * add experiments
    
    * update the experiments
    
    * update tests
    
    * update scripts
    
    * update scripts and scales
    
    * introduce bash script
    
    * run long_run (#12)

---
+++ scripts/compare_backbone/compare_backbones_train.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+source $(dirname $0)/../train.sh
+
+run_experiment resnet50v1 256 96 25 50
+run_experiment resnet50v2 256 96 25 50
+run_experiment resnet50v3 256 64 25 50
+run_experiment resnet50v4 256 8 25 50
+
+run_experiment resnet50v1 320 64 25 50
+run_experiment resnet50v2 320 64 25 50
+run_experiment resnet50v3 320 32 25 50
+run_experiment resnet50v4 320 8 25 50
+
+run_experiment resnet50v1 448 32 25 50
+run_experiment resnet50v2 448 32 25 50
+run_experiment resnet50v3 448 16 25 50
+run_experiment resnet50v4 448 6 25 50
+
+run_experiment resnet50v1 512 32 25 50
+run_experiment resnet50v2 512 24 25 50
+run_experiment resnet50v3 512 12 25 50
+run_experiment resnet50v4 512 4 25 50
---
+++ scripts/long_run/train.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+source $(dirname $0)/../train.sh
+
+run_experiment resnet50v2 512 24 25 1000
