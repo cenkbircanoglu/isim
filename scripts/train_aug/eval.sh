8e64aeee14b637563622fd52fc89fec05bb8a10f
Cenk Bircanoglu <cenk.bircanoglu@gmail.com>
2021-11-17 00:23:13 +0100

    update the scripts

--- scripts/train_aug/eval.sh
+++ scripts/train_aug/eval.sh
@@ -3,7 +3,7 @@
 BACKBONE=resnet101v2
 CAM_EVAL_THRESHOLD=0.15
 
-for model_name in 4 49 99 149 174 199 224 249 274 299 324 349 374 399 424 449; do
+for model_name in 4 24 49 74 99 124 149 174 199 224 249 274 299 324 349 374 399; do
     python -m eval_cam \
         cam_out_dir=$(pwd)/results/pipeline/${BACKBONE}/eval/make_cam/seg-model-${model_name}/cam_outputs \
         hydra.run.dir=$(pwd)/results/pipeline/${BACKBONE}/eval/eval_cam/seg-model-${model_name}_${CAM_EVAL_THRESHOLD} \
