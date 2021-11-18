f4e3e12c778580137ab3d6d016897b9ddb00e6a6
Cenk Bircanoglu <cenk.bircanoglu@gmail.com>
2021-11-18 20:07:09 +0100

    update the irnet

--- scripts/irnet/make_cam.sh
+++ scripts/irnet/make_cam.sh
@@ -3,7 +3,7 @@ BACKBONE=resnet50v2
 CROP_SIZE=512
 model_name=224
 
-python -m irnet.make_cam \
+python -m irnet.make_cam_hr \
     backbone=${BACKBONE} \
     weights=$(pwd)/results/pipeline/${BACKBONE}/train/weights/seg-model-${model_name}.pt \
     hydra.run.dir=$(pwd)/results/pipeline/${BACKBONE}/irnet/make_cam/seg-model-${model_name} \
@@ -15,7 +15,7 @@ BACKBONE=resnet101v2
 CROP_SIZE=512
 model_name=249
 
-python -m irnet.make_cam \
+python -m irnet.make_cam_hr \
     backbone=${BACKBONE} \
     weights=$(pwd)/results/pipeline/${BACKBONE}/train/weights/seg-model-${model_name}.pt \
     hydra.run.dir=$(pwd)/results/pipeline/${BACKBONE}/irnet/make_cam/seg-model-${model_name} \
