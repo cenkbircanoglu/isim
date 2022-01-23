c92fc7dbf9ee3cd24377c32ccece35f17aa8efeb
Cenk Bircanoglu <cenk.bircanoglu@gmail.com>
2022-01-23 13:14:33 +0100

    update the imp

--- README.md
+++ README.md
@@ -1,41 +1,94 @@
+[![CodeQL](https://github.com/cenkbircanoglu/isim/actions/workflows/codeql-analysis.yml/badge.svg?branch=main)](https://github.com/cenkbircanoglu/isim/actions/workflows/codeql-analysis.yml)
+
+[![pages-build-deployment](https://github.com/cenkbircanoglu/isim/actions/workflows/pages/pages-build-deployment/badge.svg?branch=gh-pages)](https://github.com/cenkbircanoglu/isim/actions/workflows/pages/pages-build-deployment)
 
 
-Python version 3.7
+[![PWC]()]()
+[![PWC]()]()
 
+# ISIM
+The official implementation of "ISIM: Iterative Self-Improved Model for Weakly Supervised Segmentation".
 
-# Train
-```bash
-python -m train
+## Citation
+- In Review for - 
+- Please cite our paper if the code is helpful to your research. [arxiv]()
+```
 ```
 
+## Abstract
+TODO
+
+## Overview
+![Overall architecture]()
+
+<br>
+
+# Prerequisite
+- Python 3.7.11, PyTorch 1.7.0, and more in requirements.txt
+- pydensecrf
+- CUDA 11.5
+- RTX 3090 GPU
+
+# Usage
+
+## Install python dependencies
+```bash
+pip install -r requirements.txt
+pip install git+https://github.com/lucasb-eyer/pydensecrf.git
+```
+
+## Download PASCAL VOC 2012 devkit
+Follow instructions in http://host.robots.ox.ac.uk/pascal/VOC/voc2012/#devkit
+
+## 1. Train an image classifier for generating CAMs
+```bash
+python -m train_cam hydra.run.dir=results/
+```
+
+## 2. Generate CAMs
+```bash
+python -m cam.make_cam hydra.run.dir=results/ weights=$(pwd)/results/weights/final-model.pt
+```
+
+## 3. Evaluate the models
+```bash
+python -m cam.eval_cam hydra.run.dir=results/ cam_out_dir=$(pwd)/results/cam_outputs/
+```
+
+## 5. Results
+Qualitative segmentation results on the PASCAL VOC 2012 validation set. 
+Top: original images. Middle: ground truth. Bottom: prediction of the segmentation model trained using the pseudo-labels from ISIM.
+![Overall architecture]()
+
+| Methods | mIoU |
+|---|---:|
+| ISIM with ResNet-101 | TODO |
+| ISIM with ResNeSt-200 | TODO |
+
+## 6. Provide the trained weights and training logs
+
+- TODO
+[experiments.zip]()
+
+- Release the final masks by our models. 
+
+| Model                  | val | test |
+|:----------------------:|:---:|:----:|
+| DeepLabv3+ ResNet-101 | [val.tgz]() | [test.tgz]() |
+| DeepLabv3+ ResNeSt-200 | [val.tgz]() | [test.tgz]() |
+
+
+### Quick Experiment on PASCAL VOC 2012 with ResNet50 backbone
 
 ```bash
-python -m make_cam \
-  encoder=${ENCODER} \
-  decoder=${DECODER} \
-  weights=$OUTPUTDIR/weights/cls-model.pt \
-  hydra.run.dir=$OUTPUTDIR/make_cam_cls \
-  output_dir=$OUTPUTDIR/make_cam_cls/cams \
-  infer_list=$(pwd)/voc12/train.txt
-
-for i in 0.05 0.1 0.15 0.2 0.25 0.3 0.35 0.4; do
-  python -m eval_cam \
-    --config-name=eval_cam \
-    cam_out_dir=$OUTPUTDIR/make_cam_cls/cams \
-    infer_set=train \
-    hydra.run.dir=$OUTPUTDIR/eval_cam_cls \
-    name=${EXPERIMENT}-cls-cam-${i} \
-    tag=$TAG \
-    project=$PROJECT \
-    cam_eval_thres=${i}
-done
-
-# EVALUATE CAM Unet
-python -m make_cam \
-  encoder=${ENCODER} \
-  decoder=${DECODER} \
-  weights=$OUTPUTDIR/weights/final-model.pt \
-  hydra.run.dir=$OUTPUTDIR/make_cam \
-  output_dir=$OUTPUTDIR/make_cam/cams \
-  infer_list=$(pwd)/voc12/train.txt
+sh scripts/run.sh
 ```
+
+| background | aeroplane | bicycle | bird | boat | bottle | bus | car | cat | chair | cow | diningtable | dog | horse | motorbike | person | pottedplant | sheep | sofa | train | tvmonitor | mIoU |
+|---|---:|---:|---:|---:|---:|---:|---:|---:|---:|---:|---:|---:|---:|---:|---:|---:|---:|---:|---:|---:|---:|
+| 0.8316 | 0.50373 | 0.3227 | 0.5934  | 0.3601 | 0.5890 | 0.6572 | 0.5765 | 0.6845 | 0.2839 | 0.5924 | 0.4507 | 0.5850 | 0.6190 | 0.6776 | 0.6606 | 0.4663 | 0.6561 | 0.5252 | 0.4804 | 0.5352 | 0.5548 |
+
+<br>
+
+For any issues, please contact <b>Cenk Bircanoglu</b>, cenk.bircanoglu@gmail.com
+
--- eval_cam.py
+++ eval_cam.py
@@ -13,6 +13,29 @@ from logger import Logger
 from utils import log_loss_summary, set_seed
 
 set_seed(3407)
+CATS = [
+    "background",
+    "aeroplane",
+    "bicycle",
+    "bird",
+    "boat",
+    "bottle",
+    "bus",
+    "car",
+    "cat",
+    "chair",
+    "cow",
+    "diningtable",
+    "dog",
+    "horse",
+    "motorbike",
+    "person",
+    "pottedplant",
+    "sheep",
+    "sofa",
+    "train",
+    "tvmonitor",
+]
 
 
 @hydra.main(config_path="./conf/", config_name="eval_cam")
@@ -24,82 +47,54 @@ def run_app(cfg: DictConfig) -> None:
         tags=["eval", "pipeline"],
     )
     logger = Logger(cfg.logs)
-    try:
-        epoch = cfg.cam_out_dir.split("/")[-2].split("-")[-1]
-        logging.info(epoch)
-        epoch = int(epoch)
-    except:
-        epoch = cfg.last_epoch
+    epochs = cfg.epochs.split(",")
     dataset = VOCSemanticSegmentationDataset(
         split=cfg.infer_set, data_dir=cfg.voc12_root
     )
     labels = [
         dataset.get_example_by_keys(i, (1,))[0] for i in range(len(dataset))
     ]
-    for subdir in os.listdir(cfg.cam_out_dir):
-        if subdir.startswith("."):
-            continue
-        folder = os.path.join(cfg.cam_out_dir, subdir)
-        preds = []
-        for id in tqdm(dataset.ids):
-            cam_dict = np.load(
-                os.path.join(folder, id + ".npy"), allow_pickle=True
-            ).item()
-            cams = cam_dict["high_res"]
-            cams = np.pad(
-                cams,
-                ((1, 0), (0, 0), (0, 0)),
-                mode="constant",
-                constant_values=cfg.cam_eval_thres,
-            )
-            keys = np.pad(cam_dict["keys"] + 1, (1, 0), mode="constant")
-            cls_labels = np.argmax(cams, axis=0)
-            cls_labels = keys[cls_labels]
-            preds.append(cls_labels.copy())
+    for iter, epoch in enumerate(epochs):
+        for subdir in os.listdir(cfg.cam_out_dir):
+            if subdir.startswith("."):
+                continue
+            folder = os.path.join(f"{cfg.cam_out_dir}-{epoch}/cam_outputs", subdir)
+            preds = []
+            for id in tqdm(dataset.ids):
+                cam_dict = np.load(
+                    os.path.join(folder, id + ".npy"), allow_pickle=True
+                ).item()
+                cams = cam_dict["high_res"]
+                cams = np.pad(
+                    cams,
+                    ((1, 0), (0, 0), (0, 0)),
+                    mode="constant",
+                    constant_values=cfg.cam_eval_thres,
+                )
+                keys = np.pad(cam_dict["keys"] + 1, (1, 0), mode="constant")
+                cls_labels = np.argmax(cams, axis=0)
+                cls_labels = keys[cls_labels]
+                preds.append(cls_labels.copy())
 
-        confusion = calc_semantic_segmentation_confusion(preds, labels)
+            confusion = calc_semantic_segmentation_confusion(preds, labels)
 
-        gtj = confusion.sum(axis=1)
-        resj = confusion.sum(axis=0)
-        gtjresj = np.diag(confusion)
-        denominator = gtj + resj - gtjresj
-        iou = gtjresj / denominator
+            gtj = confusion.sum(axis=1)
+            resj = confusion.sum(axis=0)
+            gtjresj = np.diag(confusion)
+            denominator = gtj + resj - gtjresj
+            iou = gtjresj / denominator
 
-        results = dict(
-            zip(
-                [
-                    "background",
-                    "aeroplane",
-                    "bicycle",
-                    "bird",
-                    "boat",
-                    "bottle",
-                    "bus",
-                    "car",
-                    "cat",
-                    "chair",
-                    "cow",
-                    "diningtable",
-                    "dog",
-                    "horse",
-                    "motorbike",
-                    "person",
-                    "pottedplant",
-                    "sheep",
-                    "sofa",
-                    "train",
-                    "tvmonitor",
-                ],
-                iou,
+            results = dict(zip(CATS, iou))
+            results["miou"] = np.nanmean(iou)
+            results["miou_without_background"] = np.nanmean(iou[1:])
+            logging.info(
+                f"iou: {iou}\tmiou: {np.nanmean(iou)}\tFolder: {folder}"
             )
-        )
-        results["miou"] = np.nanmean(iou)
-        results["miou_without_background"] = np.nanmean(iou[1:])
-        logging.info(f"iou: {iou}\tmiou: {np.nanmean(iou)}\tFolder: {folder}")
-        for key, value in results.items():
-            log_loss_summary(
-                logger, value, epoch, tag=f"eval_{cfg.infer_set}_{key}"
-            )
+            for key, value in results.items():
+                log_loss_summary(
+                    logger, value, iter, tag=f"eval_{cfg.infer_set}_{key}"
+                )
+    run.finish()
 
 
 if __name__ == "__main__":
